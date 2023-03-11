54b8ca81c8c4242c7908e1716f6171c9d369ec21
rajeshgourpune <rajeshgourpune@gmail.com>
2023-03-12 00:23:14 +0530

    Factory Method Pattern

---
+++ src/main/java/factoryMethodPattern/CommercialPlan.java
@@ -0,0 +1,9 @@
+package factoryMethodPattern;
+
+public class  CommercialPlan extends Plan {
+    //@override
+    public void getRate() {
+        rate = 7.50;
+    }
+}
+//end of CommercialPlan class.
