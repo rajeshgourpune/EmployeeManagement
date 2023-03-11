3a16c0fddef89fa3967376213c31a107d241f57d
rajeshgourpune <rajeshgourpune@gmail.com>
2023-03-12 00:50:54 +0530

    AbstractFactoryPattern

---
+++ src/main/java/CreationalPattern/abstractFactoryPattern/HomeLoan.java
@@ -0,0 +1,8 @@
+package CreationalPattern.abstractFactoryPattern;
+
+public class HomeLoan extends Loan{
+    public void getInterestRate(double r){
+        rate=r;
+    }
+}//End of the HomeLoan class.
+
